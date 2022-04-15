8f53b6f5f27ce5ff3a8007dad0afba08956e7c97
rohanaj2001 <rohanaj2001@gmail.com>
2022-04-15 13:28:46 +0530

    python is calculating average rgb

--- server/python_scripts/test.py
+++ server/python_scripts/test.py
@@ -1,11 +1,33 @@
 import base64
 import sys
+from traceback import print_tb
+from turtle import width
+from weakref import proxy
 import cv2
 print('hii there')
 img_data=sys.argv[1]
 print ('The image is stored for 10min only')
-with open("./shirt_images/shirtImage(Decoded).png", "wb") as fh:
+with open("./shirt_images/shirtImage.png", "wb") as fh:
   fh.write(base64.urlsafe_b64decode(img_data))
 
-# img = cv2.imread('./shirt_images/shirtImage(Decoded).png', cv2.IMREAD_UNCHANGED)
-# color = int(img[300, 300])
+path = r"./shirt_images/shirtImage.png"
+img_rgb = cv2.imread(path, 1)
+x,y,q=img_rgb.shape
+rn,gn,bn=0,0,0
+i=0
+j=0
+if(x<y):n=x
+else:n=y
+while (i<x)and(j<y):
+    r,g,b = (img_rgb[i, j])
+    rn,gn,bn=r+rn,g+gn,b+bn
+    i+=1
+    j+=1
+rn=int(rn/n)
+gn=int(gn/n)
+bn=int(bn/n)
+print('average red {rn}'.format(rn=rn))
+print('average green {gn}'.format(gn=gn))
+print('average blue {bn}'.format(bn=bn))
+# print(i)
+# print(j)
